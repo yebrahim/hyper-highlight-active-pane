53cf9f9c66959d0e453dc5a04672cc31234ff5cf
Yasser Elsayed <yasser.ebrahim@gmail.com>
2019-01-04 16:53:40 -0800

    fix css selectors, new screenshot

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ module.exports.decorateBrowserOptions = opts => {
 
 module.exports.decorateConfig = config => {
   config.css += `
-    .term_fit:not(.term_term) {
-      opacity: 0.6;
-    }
-    .term_fit.term_active {
+    .term_fit.term_active>.term_fit:not(.term_term) {
       opacity: 1;
-      transition: opacity 0.12s ease-in-out;
+      transition: opacity 0.3s ease-in-out;
       will-change: opacity;
+    }
+    .term_fit:not(.term_active)>.term_fit:not(.term_term) {
+      opacity: 0.5;
     }
   `;
   return config;
